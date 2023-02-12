7f2307d35ad72d581e5dcffad442cb5f076b50d0
artiecannv <thefiftharthur@gmail.com>
2023-02-12 06:28:13 -0700

    added code to all the routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -21,7 +21,7 @@ router.get("/:id", async (req, res) => {
     const categoryData = await Category.findByPk(req.params.id, {
       include: [Product],
     });
-    res.status(200).json(productData);
+    res.status(200).json(categoryData);
   } catch (err) {
     res.status(500).json(err);
   }
